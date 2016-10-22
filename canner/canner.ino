2cff7c3d73d425326c0e10df3f7690b009fcf113
amdeguzm <amdeguzm@ucsd.edu>
2016-10-22 13:42:18 -0800

    Added cannerdata.h struct created an instance in canner.ino globally

--- canner/canner.ino
+++ canner/canner.ino
@@ -1,5 +1,26 @@
+#include <Wire.h>
+#include <LPS.h> 
+#include <LSM6.h>
+#include <LIS3MDL.h>
+#include "cannerdata.h"
+
+LPS PTS; 
+LIS3MDL MAG;
+data_s candata; 
+
+
 void setup() {
   // put your setup code here, to run once:
+
+Serial.begin(9600);
+Wire.begin();
+
+if(! PTS.init()){
+  Serial.println("Pressure and temperature sensor failed to initialize");
+}
+if(! MAG.init()){
+  Serial.println("Magnetometer sensor failed to initialize");
+}
 
 }
 
---
+++ canner/cannerdata.h
@@ -0,0 +1,17 @@
+typedef struct packet{
+  int state;
+  float pres; 
+  float temp; 
+  float alt;
+  float airspeed; 
+  float magx;
+  float magy;
+  float magz;
+  float lat;
+  float lon;
+  float gpsalt;
+  int piccount;
+  
+  
+} data_s;
+
