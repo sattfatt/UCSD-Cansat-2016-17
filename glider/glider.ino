1641f859c60b9951e23c7d84180cc6d0f68a68c5
sattfatt <sattfatt@gmail.com>
2017-01-21 11:09:21 -0800

    Starting point for glider code

--- glider/glider.ino
+++ glider/glider.ino
@@ -1,3 +1,14 @@
+#include <Adafruit_GPS.h>
+#include <Adafruit_VC0706.h>
+#include <Wire.h>
+#include <LPS.h> 
+#include <LSM6.h>
+#include <LIS3MDL.h>
+#include "cannerdata.h"
+#include <SD.h>
+#include <math.h>
+
+
 void setup() {
   // put your setup code here, to run once:
 
